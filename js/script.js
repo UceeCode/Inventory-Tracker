dc4dd2449940650617adec81a71fedde7d757489
Nkwocha Franklin <118822651+UCEE01@users.noreply.github.com>
2023-11-29 13:53:06 +0100

    Created add method to add  to the items array

--- js/script.js
+++ js/script.js
@@ -3,4 +3,11 @@ class Inventory{
         //initializing an empty array called items
         this.items=[];
     }
+
+     //creating an add method
+     addItems(ProductName, Quantity){
+        let item = { ProductName, Quantity };
+        this.items.push(item);
+        this.renderInventory();
+    }
 }
