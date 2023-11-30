84449d2951f1f7721409e0a2da1aa648e43a9d90
Nkwocha Franklin <118822651+UCEE01@users.noreply.github.com>
2023-11-30 21:11:37 +0100

    Updated the javascript to take in prices

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ class Inventory{
     }
 
      //creating an add method
-     addItems(ProductName, Quantity){
-        let item = { ProductName, Quantity };
+     addItems(ProductName, Quantity, Price){
+        let item = { ProductName, Quantity, Price };
         this.items.push(item);
         this.renderInventory();
     }
@@ -26,12 +26,14 @@ class Inventory{
             const row = TableBody.insertRow();
             row.insertCell(0).textContent = item.ProductName;
             row.insertCell(1).textContent = item.Quantity;
+            row.insertCell(2).textContent = item.Price
+
 
             const DeleteBtn = document.createElement('button');
             DeleteBtn.textContent = 'Delete';
             DeleteBtn.onclick = () => this.deleteItems(i);
 
-            row.insertCell(2).appendChild(DeleteBtn);;
+            row.insertCell(3).appendChild(DeleteBtn);;
         });
     }
 }
@@ -41,13 +43,15 @@ const inventory = new Inventory();
 const addItems = () => {
     const ProductName = document.getElementById('productName').value;
     const ProductQuantity = document.getElementById('productQuantity').value;
+    const ProductPrice = document.getElementById('productPrice').value
 
-    if (ProductName && ProductQuantity){
-        inventory.addItems(ProductName, ProductQuantity);
+    if (ProductName && ProductQuantity && ProductPrice){
+        inventory.addItems(ProductName, ProductQuantity, ProductPrice);
 
         //to clear inputs after adding an item
         document.getElementById('productName').value = '';
         document.getElementById('productQuantity').value = '';
+        document.getElementById('productPrice').value = '';
     }
 }
  
